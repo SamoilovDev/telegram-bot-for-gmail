5105c024567162ae23821d65b13ce0ec9164161a
vladimir.samoilov <vladimir.samoilov@ecodef.cz>
2023-07-28 13:42:30 +0200

    - init commit

---
+++ src/main/java/com/samoilov/dev/telegrambotforgmail/TelegramBotForGmailApplication.java
@@ -0,0 +1,13 @@
+package com.samoilov.dev.telegrambotforgmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotForGmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotForGmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samoilov/dev/telegrambotforgmail/TelegramBotForGmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samoilov.dev.telegrambotforgmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForGmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
