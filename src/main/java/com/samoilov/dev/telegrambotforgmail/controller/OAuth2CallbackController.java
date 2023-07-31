040b8c4b5e69f08e5a538c2c56e3dd5e75af4269
vladimir.samoilov <vladimir.samoilov@ecodef.cz>
2023-08-01 00:13:31 +0200

    - implemented Google OAuth2

--- src/main/java/com/samoilov/dev/telegrambotforgmail/controller/OAuth2CallbackController.java
+++ src/main/java/com/samoilov/dev/telegrambotforgmail/controller/OAuth2CallbackController.java
@@ -5,16 +5,18 @@ import com.google.api.services.gmail.Gmail;
 import com.samoilov.dev.telegrambotforgmail.dto.GmailDto;
 import com.samoilov.dev.telegrambotforgmail.service.domain.GmailService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
+import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class OAuth2CallbackController {
@@ -24,19 +26,23 @@ public class OAuth2CallbackController {
     private final ApplicationEventPublisher eventPublisher;
 
     @GetMapping("/oauth2callback/{chatId}")
-    public ResponseEntity<Object> handleOAuth2Callback(
+    public void handleOAuth2Callback(
             @RequestParam(name = "code") String authorizationCode,
             @PathVariable(name = "chatId") Long chatId,
-            HttpServletRequest request) {
-        URI redirectUri = URI.create("https://t.me/GmailCheckerBot");
-        Credential credential = gmailService.exchangeCode(authorizationCode, request.getRequestURL().toString());
-        Gmail gmail = gmailService.createGmailService(credential);
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        try {
+            Credential credential = gmailService.exchangeCode(authorizationCode, request.getRequestURL().toString());
+            Gmail gmail = gmailService.createGmailService(credential);
 
-        eventPublisher.publishEvent(
-                GmailDto.builder().gmail(gmail).chatId(chatId).build()
-        );
+            eventPublisher.publishEvent(
+                    GmailDto.builder().gmail(gmail).chatId(chatId).build()
+            );
 
-        return ResponseEntity.created(redirectUri).build();
+            response.sendRedirect("https://t.me/GmailCheckerBot");
+        } catch (IOException e) {
+            log.error("Error while handling OAuth2 callback", e);
+        }
     }
 
 }
